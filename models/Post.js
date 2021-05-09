eb9fec1cef139b62a2befee14515ba22e333378c
bdurham227 <bdurham227@gmail.com>
2021-05-09 19:22:02 -0400

    built out post and comment models

--- models/Post.js
+++ models/Post.js
@@ -4,4 +4,49 @@ const sequelize = require('../config/connection');
 class Post extends Model{}
 
 
+Post.init(
+
+    {
+
+        id: {
+
+            type:DataTypes.INTEGER,
+
+            allowNull: false,
+
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        post_title: {
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        post_content: {
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type:DataTypes.DATE,
+        },
+        user_id: {
+            type:DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        freezeTableName: true,
+        modelName: 'post',
+    },
+);
+
+
+
+
 module.exports = Post;
