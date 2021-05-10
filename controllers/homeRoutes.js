921731ab7bbd8b82885a26d321d5ee3ebe9abb2f
bdurham227 <bdurham227@gmail.com>
2021-05-10 16:50:41 -0400

    got caught up doing css stuff forgot to commit- worked on some stylings for main.handlebars template (wasnt fun) and now have posts with user table data being rendered to the homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,29 +2,72 @@ const router = require('express').Router();
 
 const { User, Post } = require('../models');
 
-
+//get all users and their posts
 router.get('/', async (req, res) => {
     try {
-      const dbUserData = await User.findAll({
+      const dbPostData = await Post.findAll({
           
           include: [
               {
-                  model: Post,
-                  attributes: ["post_title"],
+                  model: User,
+                  attributes: ["username"],
               },
           ],
       });
+      //serialize data so the template can read it
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
 
-      const users = dbUserData.map((user) => user.get({ plain: true }));
-
-      res.status(200).json(users);
-
+    //   res.status(200).json(posts);
+    //pass serialized data into template
+      res.render('homepage', {
+          posts,
+      });
 
 
     } catch (err) {
         res.status(500).json(err)
     }
 });
+
+//get
+router.get('/posts/:id', async ( req, res) => {
+    try {
+        const dbPostData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ["username"],
+                },
+            ],
+        });
+
+        const posts = dbPostData.get({ plain: true });
+        //in the future will probably include/join comments table too
+        //res.render(dashboard)
+        res.status(200).json(posts);
+
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+
+
+
+//get request to users profile page
+// router.get('/profile', async (req, res) => {
+//     try {
+//         const dbUserData = await User.findByPk
+
+
+
+//     } catch (err) {
+//         res.status(500).json(err)
+//     }
+// });
+
+
 
 
 
